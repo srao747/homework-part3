d63a237a62a27d32b77d781540bad3cc08ad35dc
srao747 <109792471+srao747@users.noreply.github.com>
2023-03-25 23:45:39 -0400

    Add files via upload

---
+++ JavaDTD/src/DomParserDTD.java
@@ -0,0 +1,42 @@
+import java.io.File;
+
+import javax.xml.XMLConstants;
+
+import javax.xml.parsers.DocumentBuilder;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import javax.xml.transform.dom.DOMSource;
+
+import javax.xml.validation.Schema;
+
+import javax.xml.validation.SchemaFactory;
+
+
+
+import org.w3c.dom.Document;
+
+
+
+
+
+
+
+public class DomParserDTD {
+
+  public static void main(String[] args) throws Exception {
+
+    // load XML document
+
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+    DocumentBuilder builder = factory.newDocumentBuilder();
+
+    Document doc = builder.parse("shops.xml");
+    
+    System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+
+
+  }
+
+}
